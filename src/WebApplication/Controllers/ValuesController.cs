82dbdd8238a13fead72b943d71ac51538507915d
David Revoledo <davidrevoledo@d-genix.com>
2018-12-19 17:53:36 -0300

    Remove using

--- src/WebApplication/Controllers/ValuesController.cs
+++ src/WebApplication/Controllers/ValuesController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication.Controllers
